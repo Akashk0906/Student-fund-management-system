d23c60a187bedacb4462e0c10b495b7e0f408479
Akash Kushwaha <113846179+Akashk0906@users.noreply.github.com>
2025-05-10 00:04:04 +0530

    Delete program directory

--- program/tt3.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-root=Tk()
-root.geometry("1200x800")
-ak=Label(text="Loading..........",bg="black",fg="white",font=19,padx=1200)
-ak.pack(side=BOTTOM,pady=100)
-root.mainloop()
