08866fe30424cf5c4afb5f121998f9ad575740de
Akash Kushwaha <113846179+Akashk0906@users.noreply.github.com>
2025-05-10 00:06:13 +0530

    Delete Student fund/turtle1.py

--- Student fund/turtle1.py
+++
@@ -1,9 +0,0 @@
-import turtle
-colors=["red","yellow","green","pink","sky blue"]
-ak=turtle.Pen()
-turtle.bgcolor("black")
-for i in range(360):
-    ak.pencolor(colors[i%5])
-    ak.width(i/100+1)
-    ak.forward(i)
-    ak.left(80)
